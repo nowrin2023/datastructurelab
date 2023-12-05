5ab0ded8a8ecf438610fa8e1be4d2c3caac33580
nowrin2023 <147498684+nowrin2023@users.noreply.github.com>
2023-12-05 16:19:51 +0600

    Add files via upload

---
+++ doublylinkedlist1.cpp
@@ -0,0 +1,94 @@
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* prev;
+    Node* next;
+};
+
+void addFront(Node** head_ref, int new_data) {
+    Node* new_node = new Node();
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    new_node->prev = NULL;
+
+    if ((*head_ref) != NULL) {
+        (*head_ref)->prev = new_node;
+    }
+
+    (*head_ref) = new_node;
+
+    if ((*head_ref) == NULL || (*head_ref)->next == NULL) {
+        (*head_ref) = new_node;
+    }
+}
+
+void deleteNode(Node** head_ref, Node* del) {
+    if (*head_ref == NULL || del == NULL) {
+        return;
+    }
+
+    if (*head_ref == del) {
+        *head_ref = del->next;
+    }
+
+    if (del->next != NULL) {
+        del->next->prev = del->prev;
+    }
+
+    if (del->prev != NULL) {
+        del->prev->next = del->next;
+    }
+
+    delete del;
+}
+
+int main() {
+    Node* head = NULL;
+
+    addFront(&head, 100);
+    addFront(&head, 450);
+    addFront(&head, 510);
+    addFront(&head, 900);
+    addFront(&head, 300);
+
+
+    Node* nodeToDelete = head->next;
+    deleteNode(&head, nodeToDelete);
+
+
+    Node* current = head;
+    while (current != NULL) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    current = head;
+    while (current != NULL) {
+        Node* temp = current;
+        current = current->next;
+        delete temp;
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
